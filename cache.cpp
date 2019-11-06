e2e3ea8814a11ba85c078809561052502323ebe8
Chuanxiu Xiong (Ethan) <chuanxix@usc.edu>
2019-11-06 05:39:02 -0800

    LRU eviction policy

--- cache.cpp
+++ cache.cpp
@@ -1,15 +1,40 @@
 #include "cache.h"
 
-Cache::Cache(int capacity) : mCapacity(capacity)
+Cache::Cache(int capacity) : mCapacity(capacity), doublyLinkedList(), cacheData()
 {
 }
 
-std::string Cache::find(std::string &key)
+std::string Cache::find(const std::string &key)
 {
-    return std::string();
+    auto result = cacheData.find(key);
+    if (result != cacheData.end())
+    {
+        auto &it = result->second;
+        doublyLinkedList.splice(doublyLinkedList.begin(), doublyLinkedList, it);
+        return it->second;
+    }
+    return "";
 }
 
-bool Cache::upsert(std::string &key, std::string &value)
+void Cache::upsert(const std::string &key, const std::string &value)
 {
-    return false;
+    auto result = cacheData.find(key);
+    if (result != cacheData.end()) // update: does not increase the size
+    {
+        auto &it = result->second;
+        doublyLinkedList.splice(doublyLinkedList.begin(), doublyLinkedList, it);
+        it->second = value; // update
+    }
+    else // insert: could increase the size
+    {
+        if (doublyLinkedList.size() == mCapacity)
+        {
+            // remove the least recently accessed item
+            cacheData.erase(doublyLinkedList.back().first);
+            doublyLinkedList.pop_back();
+        }
+        // insert it at the front of the list (most recently accessed)
+        doublyLinkedList.emplace_front(key, value);
+        cacheData[key] = doublyLinkedList.begin();
+    }
 }
--- cache.h
+++ cache.h
@@ -1,13 +1,27 @@
 #pragma once
 #include <string>
+#include <list>
+#include <unordered_map>
 
+// Cache class - where the cached data is stored and managed.
+// The cache uses LRU eviction policy which automatically removes the least recently
+// accessed item when the data size has reached the capacity.
 class Cache
 {
 public:
     Cache(int capacity);
     std::string find(const std::string &key);
-    bool upsert(const std::string &key, const std::string &value);
+    void upsert(const std::string &key, const std::string &value);
 
 private:
-    int mCapacity;
+    unsigned int mCapacity;
+
+    // the doublyLinkedList keeps the access order of the data
+    // from the front to the back: the most recently accessed to the least recently accessed
+    // this is actually the bottom layer of the cacheData, where the value data lives
+    std::list<std::pair<std::string, std::string>> doublyLinkedList;
+
+    // the cacheData is a map that stores from the key to the keyValue pair that in the doublyLinkedList
+    // this is actually the top layer of the cacheData
+    std::unordered_map<std::string, std::list<std::pair<std::string, std::string>>::iterator> cacheData;
 };
